0b987a8628f0a2879219466c73ddc2d212c0c756
sumitbakshi <sumit.bakshi507@gmail.com>
2020-05-03 14:09:48 +0530

    Initail IoC Container

---
+++ OrgLive/OrgLive.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrgLive.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+
+            //Subscriptions
+
+            //Domain Events
+
+            //Domain Banking Commands
+
+            //Application Services
+
+            //Data
+        }
+    }
+}
